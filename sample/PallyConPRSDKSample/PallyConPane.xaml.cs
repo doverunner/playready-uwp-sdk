69b9bee3221d480f225e29bbad6b24ac5e43d16e
daniel-inka <daniel@inka.co.kr>
2022-12-06 18:14:12 +0900

    upload v1.4.0 to 1.4.x branch

--- sample/PallyConPRSDKSample/PallyConPane.xaml.cs
+++ sample/PallyConPRSDKSample/PallyConPane.xaml.cs
@@ -5,9 +5,6 @@ using Windows.UI.Xaml.Controls;
 
 namespace PallyConPRSDKSample
 {
-    /// <summary>
-    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
-    /// </summary>
     public sealed partial class PallyConPane : UserControl
     {
         public PallyConPane()
--- sample/PallyConPRSDKSample/SDKSample/HWDRM.xaml.cs
+++ sample/PallyConPRSDKSample/SDKSample/HWDRM.xaml.cs
@@ -16,13 +16,8 @@ using Windows.UI.Xaml.Navigation;
 using PallyConPRSDKSample.Model;
 using System.Diagnostics;
 
-// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
-
 namespace PallyConPRSDKSample.SDKSample
 {
-    /// <summary>
-    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
-    /// </summary>
     public sealed partial class HWDRM : Page
     {
         private IEnumerable<ContentInfo> _groups;
@@ -102,8 +97,8 @@ namespace PallyConPRSDKSample.SDKSample
                 ContentInfo info = ((Button)sender).DataContext as ContentInfo;
                 ContentInfo copy = info.Clone();
                 this.DataContext = null;
-                // Token 정보가 Hardware 라이선스를 요청하므로
-                // CustomData를 만들어 라이선스를 요청한다.
+                // Since Token information requests a Hardware license, 
+                // it creates CustomData to request a license.
                 if (copy.Token.Length > 5)
                 {
                     copy.Token = " ";
